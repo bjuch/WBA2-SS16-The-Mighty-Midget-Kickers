bea128feab0c74c086ff875b6c9c49f49673b3d5
SMWerner <s.werner94@web.de>
2016-05-04 14:46:31 +0200

    Aufgabe 1b

--- Aufgabe1.js
+++ Aufgabe1.js
@@ -1,11 +1,33 @@
 var fs = require("fs");
+var chalk = require('chalk');
+
 
 // asynchrones einlesen
 fs.readFile('wolkenkratzer.json', function (err, data) {
    if (err) {
-       return console.error(err);
+       //return console.error(err);
    }
    console.log("asynchrones einlesen: " + data.toString());
+   
+       // String in JS-Objekt umwandeln
+    
+    var obj = JSON.parse(data.toString())
+    
+    // Daten aus Wolkenkratzer entnehmen
+    
+    var Gebaude =obj.wolkenkratzer;
+    
+    // einfärben und Ausgabe
+    
+    Gebaude.forEach(function(entry){
+                    
+                    console.log(chalk.pink("Name: " +entry.name));
+                    console.log(chalk.red("Stadt: " +entry.stadt));
+                    console.log(chalk.purple("Hoehe: " +entry.hoehe));
+                    console.log("--------------");
+    
+}); 
+   
 });
 
 
