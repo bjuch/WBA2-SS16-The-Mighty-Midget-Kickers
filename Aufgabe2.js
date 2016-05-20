247bd58e908149b0544210e3e349fadb3528ddca
SMWerner <s.werner94@web.de>
2016-05-20 14:45:38 +0200

    Update Aufgabe2.js

--- Aufgabe2.js
+++ Aufgabe2.js
@@ -1,8 +1,16 @@
+var bodyParser = require ('body-parser')
+
 var app = expression();
 
 app.get('/projekt', function (req, res {
   // Liste wird ausgegeben 
-  });
+  )});
+app.post('/projekt', function (req, res {
+  res.send(jsonObject);    //Serverseitige Ausgabe eines json-Objektes
+  )});
+  
+var jsonParser = bodyParser.json();
+app.use(jsonParser)        //Serverseitiges Parsen von json
 
 
 
