fc316fc8814ac815b8c5fdc7efe039113ea5e8a5
SMWerner <s.werner94@web.de>
2016-05-23 11:13:31 +0200

    Update Aufgabe2.js

--- Aufgabe2.js
+++ Aufgabe2.js
@@ -4,12 +4,17 @@ var app = express();
 
 app.get('/projekt', function (req, res {
 res.send(jsonObject);    //Serverseitige Ausgabe eines json-Objektes
-  )});
+  }));
   
 app.post('/projekt', function (req, res {
   connection.connect();
   var projektname = req.body.name;
-  )});
+  //Tabelle wird erstellt
+  }));
+  
+app.set('/projekt/projektname', function (req, res {
+  //Veränderung des bestehenden Projekts
+  }));
   
 var jsonParser = bodyParser.json();
 app.use(jsonParser)        //Serverseitiges Parsen von json
