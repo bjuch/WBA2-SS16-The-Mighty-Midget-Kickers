fcae12313b241c9792c8f5bba77cbea0e0d21f4f
bjuch <benjamin.juch@gmail.com>
2016-05-25 15:29:46 +0200

    fertig stellen

--- workshop2/Aufgabe2.js
+++ workshop2/Aufgabe2.js
@@ -2,37 +2,33 @@ var express = require('express');
 var bodyParser = require ('body-parser');
 var app = express();
 var jsonParser = bodyParser.json();
-
 app.use(jsonParser);       //Serverseitiges Parsen von json
 
 
+
+
 app.get('/projekt/', jsonParser, function (req, res) {
-  var objekt = JSON.parse(erfolgstabelle.json);
-  document.write(erfolgstabelle[2].name);
+  var objekt = JSON.parse(erfolgstabelle.json)
+  document.write(erfolgstabelle[2].name)
   });
 
-  
+
 app.post('/projekt',jsonParser,function (req, res) {
   connection.connect();
   var projektname = req.body.name;
+  var objekt = JSON.parse(erfolgstabelle.json)
+    
+    res.end(erfolgstabelle.toString());
   
-  express.readFile('erfolgstabelle.json', function (err, data)){
-  if (err){
-    console.log(err);
-  }
-  else{
-  res.end(data.toString());
-  }
-  }
-  });
   
-app.get('/projekt', function (req, res) {
-  res.write("Beispielhafte Ausgabe eines Teils des Json-Objekts erfolgstabelle");
-  document.write('Name einer Aufgabe: ', erfolgstabelle[1].name);
+                   });
+
+app.get('/projekt',jsonParser, function (req, res){ 
+ var objekt = JSON.parse(erfolgstabelle.json);
+    
+    res.write("Beispielhafte Ausgabe eines Teils des Json-Objekts erfolgstabelle");
+  document.write('Name einer Aufgabe: ',erfolgstabelle[1].name);
   res.end();
 });
-
-
-
 
 app.listen(3000);
